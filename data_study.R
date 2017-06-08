5ac93d1948d85fa143bb73142bd2e7f16c45ceaf
Igor Manzhos <manzhos.igor@gmail.com>
2017-06-08 07:42:33 +0300

    code refactoring

--- data_study.R
+++ data_study.R
@@ -22,6 +22,11 @@ orders %>%
   group_by(user_id, product_id) %>%
   count(product_id) -> userRating
 
+# Тренировочный набор данных с рейтингами
+trainSet <-
+  data_memory(userRating$user_id, userRating$product_id, rating = userRating$n, index1 = TRUE)
+r = Reco()  # инициализация
+
 startTimeCalculation <- proc.time()
 opts = r$tune(
   trainSet,
@@ -52,52 +57,32 @@ opts = r$tune(
   )
 )
 startTimeCalculation - proc.time()
+# user     system    elapsed 
+# -26344.240    -79.609  -6713.053 
+opts$res
 
-opts
-# Тренировочный набор данных с рейтингами
-trainSet <-
-  data_memory(userRating$user_id, userRating$product_id, rating = userRating$n)
 
-r = Reco()  # инициализация
 
 # Тренируем с параметрами по умолчанию
 startTimeCalculation <- proc.time()
-r$train(trainSet) 
+r$train(trainSet, opts = c(opts$min, nthread = 4, niter = 20,nmf = TRUE)) 
 startTimeCalculation - proc.time()
 # user  system elapsed 
-# -26.252  -0.579 -26.857 
+# -54.275  -1.047 -12.573 
 
-# iter      tr_rmse          obj
-# 0       3.2347   1.5385e+08
-# 1       3.1000   1.4261e+08
-# 2       3.0553   1.3925e+08
-# 3       3.0249   1.3719e+08
-# 4       2.9993   1.3548e+08
-# 5       2.9770   1.3402e+08
-# 6       2.9575   1.3276e+08
-# 7       2.9402   1.3168e+08
-# 8       2.9241   1.3070e+08
-# 9       2.9089   1.2975e+08
-# 10       2.8950   1.2893e+08
-# 11       2.8815   1.2813e+08
-# 12       2.8685   1.2737e+08
-# 13       2.8564   1.2667e+08
-# 14       2.8447   1.2600e+08
-# 15       2.8335   1.2535e+08
-# 16       2.8230   1.2475e+08
-# 17       2.8128   1.2416e+08
-# 18       2.8035   1.2366e+08
-# 19       2.7943   1.2314e+08
 
 testSet <- data_memory(userRating$user_id[1:10000], userRating$product_id[1:10000])
 predictionSet <- r$predict(testSet, out_memory())
 
 sqrt(sum((predictionSet-userRating$n[1:10000])^2))/10000
 # [1] 0.0244997
+# after tuning 0.02482675
+# [1] 0.02505054 if nmf = TRUE - non negative 
 
 ## Write P and Q matrices to files
-P_file = out_file(tempfile())
-Q_file = out_file(tempfile())
+P_file = out_file("p_out.txt")
+Q_file = out_file("q_out.txt")
 r$output(P_file, Q_file)
-head(read.table(P_file@dest, header = FALSE, sep = " "))
-head(read.table(Q_file@dest, header = FALSE, sep = " "))
+Pmatrix <- read.table(P_file@dest, header = FALSE, sep = " ")
+Qmatrix <- read.table(Q_file@dest, header = FALSE, sep = " ")
+
